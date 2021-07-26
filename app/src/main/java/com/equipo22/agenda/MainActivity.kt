34bad5e2280feae1fe0591250504699998155fc0
VogelCoage <bladburn@gmail.com>
2021-07-25 20:42:33 -0500

    Pantalla de registro lista, con el botón de registro enlazado con la activity correspondiente

--- app/src/main/java/com/equipo22/agenda/MainActivity.kt
+++ app/src/main/java/com/equipo22/agenda/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.equipo22.agenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnLogIn: Button
+    private lateinit var btnSingUp: Button
+    private lateinit var txtUsr: EditText
+    private lateinit var txtPass: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogIn = findViewById(R.id.btnLogIn)
+        btnSingUp = findViewById(R.id.btnSignUp)
+
+        btnSingUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
