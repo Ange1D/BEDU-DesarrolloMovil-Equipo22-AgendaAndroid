a8018a6f8393951046195ffe9c94158f6a8ee756
Braulio Hernández Palagot <bdpalagot@gmail.com>
2021-07-28 17:41:48 -0500

    Actualización VerTareas y Configuracion
    Cambios realizados:
        1) Se corrigieron detalles que impedían la correcta ejecución de las activities VerTareasActivity.kt y ConfiguracionActivity.kt.
        2) Se actualizó el background de dichas activities para que concordaran con el del resto de la app.

---
+++ app/src/main/java/com/equipo22/agenda/ConfiguracionActivity.kt
@@ -0,0 +1,11 @@
+package com.equipo22.agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfiguracionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracion)
+    }
+}
