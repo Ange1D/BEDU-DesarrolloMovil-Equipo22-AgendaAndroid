f33ee42b2f8ea7691e883694a1ad8fb931739452
Ange1D <47231898+Ange1D@users.noreply.github.com>
2021-07-28 18:42:19 -0500

    add spinner

--- app/src/main/java/com/equipo22/agenda/AgregarTareaActivity.kt
+++ app/src/main/java/com/equipo22/agenda/AgregarTareaActivity.kt
@@ -3,11 +3,17 @@ package com.equipo22.agenda
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 
-class AgregarTareaActivity : AppCompatActivity() {
+class AgregarTareaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var btnSave: Button
+
+
+    var tareaprevia:Array<String> = arrayOf("Tarea 1","Tarea 2")
+    var frecuencia:Array<String> = arrayOf("Una sola vez","Todos los Lunes","Todos los Martes")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +21,69 @@ class AgregarTareaActivity : AppCompatActivity() {
 
         btnSave = findViewById(R.id.btnSave)
 
+        val spinner: Spinner = findViewById(R.id.tareaPrevia)
+        val spinner2: Spinner = findViewById(R.id.frecuencia)
+
+        ArrayAdapter(this, android.R.layout.simple_spinner_item, tareaprevia)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(applicationContext ,tareaprevia[position] , Toast.LENGTH_LONG).show()
+            }
+        }
+
+        ArrayAdapter(this, android.R.layout.simple_spinner_item, frecuencia)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner2.adapter = adapter
+            }
+
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext ,frecuencia[position] , Toast.LENGTH_LONG).show()
+            }
+        }
+
+
         btnSave.setOnClickListener {
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
     }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        showDialog("No seleccionaste idioma","Vuelve a desplegar la lista y asegúrate de elegir correctamente a alguna")
+        Toast.makeText(applicationContext, "No idioma" , Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun showDialog(title:String,message:String){
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK"){dialogInterface, which -> }
+            .create()
+            .show()
+    }
+
 }
--- app/src/main/java/com/equipo22/agenda/EditarTareaActivity.kt
+++ app/src/main/java/com/equipo22/agenda/EditarTareaActivity.kt
@@ -3,12 +3,19 @@ package com.equipo22.agenda
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 
-class EditarTareaActivity : AppCompatActivity() {
+class EditarTareaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var btnSave: Button
     private lateinit var btnCancel: Button
+
+
+    var tareaprevia:Array<String> = arrayOf("Tarea 1","Tarea 2")
+    var frecuencia:Array<String> = arrayOf("Una sola vez","Todos los Lunes","Todos los Martes")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,53 @@ class EditarTareaActivity : AppCompatActivity() {
 
         btnSave = findViewById(R.id.btnSave)
         btnCancel = findViewById(R.id.btnCancel)
+
+        val spinner: Spinner = findViewById(R.id.tareaPrevia)
+        val spinner2: Spinner = findViewById(R.id.frecuencia)
+
+        ArrayAdapter(this, android.R.layout.simple_spinner_item, tareaprevia)
+            .also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext ,tareaprevia[position] , Toast.LENGTH_LONG).show()
+            }
+        }
+
+        ArrayAdapter(this, android.R.layout.simple_spinner_item, frecuencia)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner2.adapter = adapter
+            }
+
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext ,frecuencia[position] , Toast.LENGTH_LONG).show()
+            }
+        }
+
+
 
         btnSave.setOnClickListener {
             val intent = Intent(this, MenuActivity::class.java)
@@ -27,4 +81,40 @@ class EditarTareaActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+
+            when (view.getId()) {
+                R.id.rb_pendiente ->
+                    if (checked) {
+
+                    }
+                R.id.rb_finalizado ->
+                    if (checked) {
+
+                    }
+            }
+        }
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        showDialog("No seleccionaste idioma","Vuelve a desplegar la lista y asegúrate de elegir correctamente a alguna")
+        Toast.makeText(applicationContext, "No idioma" , Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun showDialog(title:String,message:String){
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK"){dialogInterface, which -> }
+            .create()
+            .show()
+    }
+
 }
