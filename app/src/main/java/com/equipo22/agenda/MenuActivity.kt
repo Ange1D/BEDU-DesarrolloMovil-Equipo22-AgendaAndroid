e1c5b966693bc85951845d1c2c62f3f1fa7501b8
Ange1D <47231898+Ange1D@users.noreply.github.com>
2021-07-29 09:37:34 -0500

    delete btnEdit

--- app/src/main/java/com/equipo22/agenda/MenuActivity.kt
+++ app/src/main/java/com/equipo22/agenda/MenuActivity.kt
@@ -9,7 +9,6 @@ class MenuActivity : AppCompatActivity() {
 
     private lateinit var btnShow: Button
     private lateinit var btnAdd: Button
-    private lateinit var btnEdit: Button
     private lateinit var btnConfig: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +17,6 @@ class MenuActivity : AppCompatActivity() {
 
         btnShow = findViewById(R.id.btnShow)
         btnAdd = findViewById(R.id.btnAdd)
-        btnEdit = findViewById(R.id.btnEdit)
         btnConfig = findViewById(R.id.btnConfig)
 
         btnAdd.setOnClickListener {
@@ -26,10 +24,6 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        btnEdit.setOnClickListener {
-            val intent = Intent(this, EditarTareaActivity::class.java)
-            startActivity(intent)
-        }
         
         btnShow.setOnClickListener {
             val intent = Intent(this, VerTareasActivity::class.java)
