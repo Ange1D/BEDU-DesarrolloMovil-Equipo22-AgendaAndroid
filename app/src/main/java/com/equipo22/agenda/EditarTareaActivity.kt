9679d0b2e3c89ea4a4c531b976f960ea5316bf6e
Braulio Hernández Palagot <bdpalagot@gmail.com>
2021-08-05 23:52:39 -0500

    Implemetación DatePicker y TimePicker
    Cambios realizados:
        1) Se implementó un DatePicker de Material Design para la selección de fecha de inicio y fin en EditarTareaActivity.kt.
        2) Se implementó un TimePicker de Material Design para la selección de hora de inicio y fin en EditarTareaActivity.kt.
        3) El campo de prioridad se modificó para funcionar como un dropdown.

--- app/src/main/java/com/equipo22/agenda/EditarTareaActivity.kt
+++ app/src/main/java/com/equipo22/agenda/EditarTareaActivity.kt
@@ -6,42 +6,98 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class EditarTareaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var btnSave: Button
     private lateinit var btnCancel: Button
-    private lateinit var txtTitle: EditText
-    private lateinit var txtHour: EditText
+    private lateinit var txtTitulo: EditText
+    private lateinit var txtDescripcion: EditText
+    private lateinit var txtFechaInicio: TextInputEditText
+    private lateinit var txtHoraInicio: TextInputEditText
+    private lateinit var txtFechaFin: TextInputEditText
+    private lateinit var txtHoraFin: TextInputEditText
 
-    var tareaprevia = listOf("Tarea 1","Tarea 2")
-    var frecuencia:Array<String> = arrayOf("Una sola vez","Todos los Lunes","Todos los Martes")
-
+    var tareaprevia = listOf("Tarea 1", "Tarea 2")
+    var frecuencia = listOf("Una sola vez", "Todos los Lunes", "Todos los Martes")
+    var prioridad = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editar_tarea)
 
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText(resources.getString(R.string.fecha))
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+        datePicker.addOnPositiveButtonClickListener {
+            when {
+                txtFechaInicio.hasFocus() -> {
+                    txtFechaInicio.setText(
+                        datePicker.headerText
+                            .replace(" ", "/")
+                    )
+                }
+                txtFechaFin.hasFocus() -> {
+                    txtFechaFin.setText(
+                        datePicker.headerText
+                            .replace(" ", "/")
+                    )
+                }
+            }
+        }
+
+        val timePicker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText(resources.getString(R.string.hora))
+                .build()
+        timePicker.addOnPositiveButtonClickListener {
+            when {
+                txtHoraInicio.hasFocus() -> {
+                    txtHoraInicio.setText("${timePicker.hour}:${timePicker.minute} hrs.")
+                }
+                txtHoraFin.hasFocus() -> {
+                    txtHoraFin.setText("${timePicker.hour}:${timePicker.minute} hrs.")
+                }
+            }
+        }
+
         btnSave = findViewById(R.id.btnSave)
         btnCancel = findViewById(R.id.btnCancel)
+        txtFechaInicio = findViewById(R.id.fechaInicio)
+        txtHoraInicio = findViewById(R.id.horaInicio)
+        txtFechaFin = findViewById(R.id.fechaFin)
+        txtHoraFin = findViewById(R.id.horaFin)
 
-        val spinner:AutoCompleteTextView = findViewById(R.id.tareaPrevia)
+        val spinner: AutoCompleteTextView = findViewById(R.id.tareaPrevia)
         val spinner2: AutoCompleteTextView = findViewById(R.id.frecuencia)
-        txtTitle = findViewById(R.id.titulo)
-        txtHour = findViewById(R.id.descripcion)
+        val spinner3: AutoCompleteTextView = findViewById(R.id.prioridad)
+        txtTitulo = findViewById(R.id.titulo)
+        txtDescripcion = findViewById(R.id.descripcion)
         val title = intent.getStringExtra("Titulo")
         val hour = intent.getStringExtra("Horario")
 
         if (title != null && hour != null) {
-            txtTitle.setText(title)
-            txtHour.setText(hour)
+            txtTitulo.setText(title)
+            txtDescripcion.setText(hour)
         }
 
         val previaAdapter = ArrayAdapter(this, R.layout.dropdown_item, tareaprevia)
         spinner.setAdapter(previaAdapter)
 
         val frecuenciaAdapter = ArrayAdapter(this, R.layout.dropdown_item, frecuencia)
-        spinner2.setAdapter(previaAdapter)
+        spinner2.setAdapter(frecuenciaAdapter)
+
+        val prioridadAdapter = ArrayAdapter(this, R.layout.dropdown_item, prioridad)
+        spinner3.setAdapter(prioridadAdapter)
 
         btnSave.setOnClickListener {
             val intent = Intent(this, VerTareasActivity::class.java)
@@ -51,6 +107,35 @@ class EditarTareaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
         btnCancel.setOnClickListener {
             val intent = Intent(this, VerTareasActivity::class.java)
             startActivity(intent)
+        }
+
+        txtFechaInicio.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus)
+                datePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtFechaInicio.setOnClickListener {
+            datePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtFechaFin.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus)
+                datePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtFechaInicio.setOnClickListener {
+            datePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtHoraInicio.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus)
+                timePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtHoraInicio.setOnClickListener {
+            timePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtHoraFin.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus)
+                timePicker.show(supportFragmentManager, "Fecha")
+        }
+        txtHoraInicio.setOnClickListener {
+            timePicker.show(supportFragmentManager, "Fecha")
         }
     }
 
@@ -72,21 +157,23 @@ class EditarTareaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
     }
 
 
-
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        showDialog("No seleccionaste idioma","Vuelve a desplegar la lista y asegúrate de elegir correctamente a alguna")
-        Toast.makeText(applicationContext, "No idioma" , Toast.LENGTH_LONG).show()
+        showDialog(
+            "No seleccionaste idioma",
+            "Vuelve a desplegar la lista y asegúrate de elegir correctamente a alguna"
+        )
+        Toast.makeText(applicationContext, "No idioma", Toast.LENGTH_LONG).show()
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
         TODO("Not yet implemented")
     }
 
-    private fun showDialog(title:String,message:String){
+    private fun showDialog(title: String, message: String) {
         AlertDialog.Builder(this)
             .setTitle(title)
             .setMessage(message)
-            .setPositiveButton("OK"){dialogInterface, which -> }
+            .setPositiveButton("OK") { dialogInterface, which -> }
             .create()
             .show()
     }
