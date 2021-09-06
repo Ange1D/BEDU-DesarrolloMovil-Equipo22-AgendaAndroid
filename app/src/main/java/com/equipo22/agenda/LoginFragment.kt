f163af534543053ea5975735deac1cf62c7de323
VogelCoage <bladburn@gmail.com>
2021-09-06 14:18:03 -0500

    Transición de fragmentos login a signup

--- app/src/main/java/com/equipo22/agenda/LoginFragment.kt
+++ app/src/main/java/com/equipo22/agenda/LoginFragment.kt
@@ -20,10 +20,11 @@ import com.equipo22.agenda.tareas.TareaManagementActivity
 class LoginFragment : Fragment() {
     var usuarios: MutableList<Usuario> = ArrayList()
     private lateinit var binding: LoginFragmentBinding
+    private var started: Boolean = false
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,98 +35,97 @@ class LoginFragment : Fragment() {
 
         usuarios.add(Usuario("usuario", "mailito@neo.com", "contraseña"))
 
-        binding.btnLogIn.isEnabled = false
-        binding.btnLogIn.setTextColor(ContextCompat.getColor(requireContext(), R.color.textDisabled))
+        with (binding) {
+            btnLogIn.isEnabled = false
+            btnLogIn.setTextColor(ContextCompat.getColor(requireContext(), R.color.textDisabled))
 
-        intro()
+            intro()
+            started = true
 
-        //Se verifica que los inputs tengan datos y sólo entonces se habilita el botón para el login
-        binding.inputTextUsr.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {}
-            override fun beforeTextChanged(
-                s: CharSequence, start: Int,
-                count: Int, after: Int
-            ) {
-            }
-
-            override fun onTextChanged(
-                s: CharSequence, start: Int,
-                before: Int, count: Int
-            ) {
-                with (binding) {
-                    if (inputTextUsr.text.toString().isNotEmpty()) {
-                        btnLogIn.isEnabled = true
-                        btnLogIn.setTextColor(ContextCompat.getColor(context!!, R.color.secondaryTextColor))
-                    }
+            //Se verifica que los inputs tengan datos y sólo entonces se habilita el botón para el login
+            inputTextUsr.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable) {}
+                override fun beforeTextChanged(
+                    s: CharSequence, start: Int,
+                    count: Int, after: Int
+                ) {
                 }
 
-            }
-        })
+                override fun onTextChanged(
+                    s: CharSequence, start: Int,
+                    before: Int, count: Int
+                ) {
+                    with (binding) {
+                        if (inputTextUsr.text.toString().isNotEmpty()) {
+                            btnLogIn.isEnabled = true
+                            btnLogIn.setTextColor(ContextCompat.getColor(context!!, R.color.secondaryTextColor))
+                        }
+                    }
 
-        binding.inputTextPass.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?, start: Int,
-                count: Int, after: Int
-            ) { }
+                }
+            })
 
-            override fun onTextChanged(
-                s: CharSequence?, start: Int,
-                before: Int, count: Int
-            ) {
-                with (binding) {
+            inputTextPass.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?, start: Int,
+                    count: Int, after: Int
+                ) { }
+
+                override fun onTextChanged(
+                    s: CharSequence?, start: Int,
+                    before: Int, count: Int
+                ) {
                     btnLogIn.isEnabled = true
                     btnLogIn.setTextColor(ContextCompat.getColor(context!!, R.color.secondaryTextColor))
+
                 }
-            }
 
-            override fun afterTextChanged(s: Editable?) { }
+                override fun afterTextChanged(s: Editable?) { }
 
-        })
-        //--------------------------------------------------------------------------------
-        //listener del botón de login
-        binding.btnLogIn.setOnClickListener {
-            if (binding.inputTextUsr.text.toString().equals(usuarios[0].nombre) && (binding.inputTextPass.text.toString()
-                    .equals(usuarios[0].password))
-            ) {
-                val intent = Intent(context, TareaManagementActivity::class.java)
-                startActivity(intent)
-                preferences.edit()
-                    .putBoolean(IS_LOGGED, true)
-                    .apply()
-                requireActivity().finish()
-            }
-            else if (binding.inputTextUsr.text.toString().equals(usuarios[0].nombre) && (!binding.inputTextPass.text.toString().equals(usuarios[0].password))) {
-                //Se cambia el texto de la view
-                    with (binding) {
-                        lostPass.text = getString(R.string.lostPass)
-                        inputTextUsr.text?.clear()
-                        inputTextPass.text?.clear()
-                        personName.error = null
-                        textPassword.error = getString(R.string.passIncorrect)
+            })
+            //--------------------------------------------------------------------------------
+            //listener del botón de login
+            btnLogIn.setOnClickListener {
+                if (inputTextUsr.text.toString() == usuarios[0].nombre && (inputTextPass.text.toString() == usuarios[0].password)
+                ) {
+                    val intent = Intent(context, TareaManagementActivity::class.java)
+                    startActivity(intent)
+                    preferences.edit()
+                        .putBoolean(IS_LOGGED, true)
+                        .apply()
+                    requireActivity().finish()
+                }
+                else if (inputTextUsr.text.toString() == usuarios[0].nombre && (inputTextPass.text.toString() != usuarios[0].password)) {
+                    //Se cambia el texto de la view
+                    lostPass.text = getString(R.string.lostPass)
+                    inputTextUsr.text?.clear()
+                    inputTextPass.text?.clear()
+                    personName.error = null
+                    textPassword.error = getString(R.string.passIncorrect)
+
+                    Toast.makeText(
+                        context,
+                        resources.getString(R.string.msgFailLogin), Toast.LENGTH_LONG
+                    ).show()
+                    //se agrega el listener a la view (solo en caso de que la contraseña no sea válida)
+                    lostPass.setOnClickListener {
+                        (activity as MainActivity).navigateTo(ResetPassFragment(), false)
                     }
-                Toast.makeText(
-                    context,
-                    resources.getString(R.string.msgFailLogin), Toast.LENGTH_LONG
-                ).show()
-                //se agrega el listener a la view (solo en caso de que la contraseña no sea válida)
-                binding.lostPass.setOnClickListener {
-                    (activity as MainActivity).navigateTo(ResetPassFragment(), false)
                 }
-            }
-            else if (!binding.inputTextUsr.text.toString().equals(usuarios[0].nombre) && (binding.inputTextPass.text.toString().equals(usuarios[0].password))) {
-                with (binding){
+                else if (inputTextUsr.text.toString() != usuarios[0].nombre && (inputTextPass.text.toString() == usuarios[0].password)) {
+
                     inputTextUsr.text?.clear()
                     inputTextPass.text?.clear()
                     personName.error = getString(R.string.nameIncorrect)
                     textPassword.error = null
+
+                    Toast.makeText(
+                        context,
+                        resources.getString(R.string.msgFailLogin), Toast.LENGTH_LONG
+                    ).show()
                 }
-                Toast.makeText(
-                    context,
-                    resources.getString(R.string.msgFailLogin), Toast.LENGTH_LONG
-                ).show()
-            }
-            else {
-                with (binding) {
+                else {
+
                     lostPass.text = getString(R.string.lostPass)
                     inputTextUsr.text?.clear()
                     inputTextPass.text?.clear()
@@ -136,22 +136,36 @@ class LoginFragment : Fragment() {
                         (activity as MainActivity).navigateTo(ResetPassFragment(), false)
 
                     }
+
                 }
             }
+            //Listener del botón de signup
+            btnSignUp.setOnClickListener {
+                //(activity as MainActivity).navigateTo(SignupFragment(), false)
+                (activity as MainActivity).supportFragmentManager
+                    .beginTransaction()
+                    .addSharedElement(appLogo, appLogo.transitionName)
+                    .addSharedElement(personName, personName.transitionName)
+                    .addSharedElement(inputTextUsr, inputTextUsr.transitionName)
+                    .addSharedElement(inputTextPass, inputTextPass.transitionName)
+                    .addSharedElement(textPassword, textPassword.transitionName)
+                    .addToBackStack(null)
+                    .replace(R.id.fragment_container, SignupFragment())
+                    .commit()
+            }
         }
-        //Listener del botón de signup
-        binding.btnSignUp.setOnClickListener {
-            //(activity as MainActivity).navigateTo(ResetPassFragment(), false)
-            (activity as MainActivity).navigateTo(SignupFragment(), false)
-        }
+
+
         return view
     }
 
     private fun intro () {
         with (binding) {
-            AnimatorInflater.loadAnimator(context, R.animator.intro_up).apply {
-                setTarget(appLogo)
-                start()
+            if (!started) {
+                AnimatorInflater.loadAnimator(context, R.animator.intro_up).apply {
+                    setTarget(appLogo)
+                    start()
+                }
             }
             AnimatorInflater.loadAnimator(context, R.animator.intro_down).apply {
                 setTarget(btnLogIn)
@@ -184,13 +198,6 @@ class LoginFragment : Fragment() {
         }
 
     }
-
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val logo = binding.appLogo
-        ViewCompat.setTransitionName(logo, "logo")
-
-    }*/
 
 }
 
